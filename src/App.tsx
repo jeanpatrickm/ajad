d52502f53dd850afae7f648c2d911ccdd67a74ca
diegorezm <dmourarezende@gmail.com>
2026-03-11 15:27:34 -0300

    new SEO tags

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,23 @@ export default function App() {
   return (
     <>
       <Helmet>
-        <title>Ana Júlia</title>
-        <meta name="description" content="Descrição" />
+        <title>Ana Julia | Advogada em Vale do Paraíba</title>
+        <meta name="description" content="Advogada em Taubaté, Pindamonhangaba, São José dos Campos e região do Vale do Paraíba. Entre em contato para uma consulta." />
+
+
+        {/* Favicon */}
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+
+        {/* Local SEO */}
+        <meta name="geo.region" content="BR-SP" />
+        <meta name="geo.placename" content="Vale do Paraíba, São Paulo" />
+
+        {/* Open Graph */}
+        <meta property="og:title" content="Ana Julia | Advogada em Vale do Paraíba" />
+        <meta property="og:description" content="Advogada em Taubaté, Pindamonhangaba, São José dos Campos e região do Vale do Paraíba." />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="pt_BR" />
+        {/* <meta property="og:url" content="https://anaJulia.com.br" /> */}
       </Helmet>
 
       <Index />
